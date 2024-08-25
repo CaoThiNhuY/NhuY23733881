a94865dc148cf3a873611ced41d1ceda682a1923
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-25 11:45:30 +0700

    Add files via upload

---
+++ chuoidaihoc.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 11:16:03 2024
+
+@author: Admin
+"""
+#bai1
+chuoi = "Dai hoc quoc gia, Khu pho 6, P.Linh Trung, Q.Thu duc, Tp.HCM"
+print(chuoi.replace(", ","\n"))
+#bai2
+chuoi = "Dai hoc quoc gia, Khu pho 6, P.Linh Trung, Q.Thu duc, Tp.HCM"
+print(chuoi.replace(", ","\n").replace("P. ","").replace("Q.","").replace("Tp.",""))
+
+    
